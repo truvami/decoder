105301104dbe16e72599e6278f81076c93b550d9
TAASMST2 <stefan.smiljanic1@swisscom.com>
2024-10-25 13:05:38 +0200

    Fixed missing return values for Nomad XL Decoder.

--- pkg/decoder/nomadxl/v1/decoder.go
+++ pkg/decoder/nomadxl/v1/decoder.go
@@ -68,16 +68,16 @@ func (t NomadXLv1Decoder) getConfig(port int16) (decoder.PayloadConfig, error) {
 	return decoder.PayloadConfig{}, fmt.Errorf("port %v not supported", port)
 }
 
-func (t NomadXLv1Decoder) Decode(data string, port int16, devEui string) (interface{}, error) {
+func (t NomadXLv1Decoder) Decode(data string, port int16, devEui string) (interface{}, interface{}, error) {
 	config, err := t.getConfig(port)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	decodedData, err := helpers.Parse(data, config)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return decodedData, nil
+	return decodedData, nil, nil
 }
