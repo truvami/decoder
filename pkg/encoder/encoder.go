c7d4f5a6e4fa083a93c494b1ba2cd01d6cfeb29f
simylein <67859356+simylein@users.noreply.github.com>
2025-01-24 18:27:48 +0000

    chore(encoder): no more useless types

--- pkg/encoder/encoder.go
+++ pkg/encoder/encoder.go
@@ -1,24 +1,5 @@
 package encoder
 
-import "reflect"
-
-// FieldConfig defines the structure of a single field in the payload
-type FieldConfig struct {
-	Name      string
-	Start     int
-	Length    int
-	Transform func(interface{}) interface{}
-	Optional  bool
-	Hex       bool
-}
-
-// PayloadConfig defines the overall structure of the payload, including the target struct type
-type PayloadConfig struct {
-	Fields          []FieldConfig
-	TargetType      reflect.Type
-	StatusByteIndex *int // can be nil
-}
-
 type Encoder interface {
 	Encode(interface{}, int16, string) (interface{}, interface{}, error)
 }
