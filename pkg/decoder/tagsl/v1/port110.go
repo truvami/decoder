b17a1c3bcc81cb2cddad77f556361d3c46b0ed46
simylein <67859356+simylein@users.noreply.github.com>
2025-04-23 08:46:33 +0000

    fix(tagsl): port 10 and 110 optional ttf pdop and satellites behavior

--- pkg/decoder/tagsl/v1/port110.go
+++ pkg/decoder/tagsl/v1/port110.go
@@ -1,6 +1,8 @@
 package tagsl
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/truvami/decoder/pkg/decoder"
@@ -16,17 +18,34 @@ import (
 // | 11-12 | 2    | Altitude                                  | uint16, 1/10 meter     |
 // | 11-14 | 4    | Unix timestamp                            | uint32                 |
 // | 17-18 | 2    | Battery voltage                           | uint16, mV             |
+// | 19    | 1    | Time to fix                               | uint8, s               |
+// | 20    | 1    | Position dilution of precision            | uint8, 0.5m            |
+// | 21    | 1    | Number of satellites                      | uint8                  |
 // +-------+------+-------------------------------------------+------------------------+
 
 type Port110Payload struct {
-	Moving      bool      `json:"moving"`
-	DutyCycle   bool      `json:"dutyCycle"`
-	BufferLevel uint16    `json:"bufferLevel"`
-	Latitude    float64   `json:"latitude" validate:"gte=-90,lte=90"`
-	Longitude   float64   `json:"longitude" validate:"gte=-180,lte=180"`
-	Altitude    float64   `json:"altitude"`
-	Timestamp   time.Time `json:"timestamp"`
-	Battery     float64   `json:"battery" validate:"gte=1,lte=5"`
+	Moving      bool          `json:"moving"`
+	DutyCycle   bool          `json:"dutyCycle"`
+	BufferLevel uint16        `json:"bufferLevel"`
+	Latitude    float64       `json:"latitude" validate:"gte=-90,lte=90"`
+	Longitude   float64       `json:"longitude" validate:"gte=-180,lte=180"`
+	Altitude    float64       `json:"altitude"`
+	Timestamp   time.Time     `json:"timestamp"`
+	Battery     float64       `json:"battery" validate:"gte=1,lte=5"`
+	TTF         time.Duration `json:"ttf"`
+	PDOP        float64       `json:"pdop"`
+	Satellites  uint8         `json:"satellites" validate:"gte=3,lte=27"`
+}
+
+func (p Port110Payload) MarshalJSON() ([]byte, error) {
+	type Alias Port110Payload
+	return json.Marshal(&struct {
+		*Alias
+		TTF string `json:"ttf"`
+	}{
+		Alias: (*Alias)(&p),
+		TTF:   fmt.Sprintf("%.0fs", p.TTF.Seconds()),
+	})
 }
 
 var _ decoder.UplinkFeatureBase = &Port110Payload{}
@@ -57,14 +76,23 @@ func (p Port110Payload) GetAccuracy() *float64 {
 }
 
 func (p Port110Payload) GetTTF() *time.Duration {
-	return nil
-}
-
-func (p Port110Payload) GetSatellites() *uint8 {
+	if p.TTF.Nanoseconds() != 0 {
+		return &p.TTF
+	}
 	return nil
 }
 
 func (p Port110Payload) GetPDOP() *float64 {
+	if p.PDOP != 0 {
+		return &p.PDOP
+	}
+	return nil
+}
+
+func (p Port110Payload) GetSatellites() *uint8 {
+	if p.Satellites != 0 {
+		return &p.Satellites
+	}
 	return nil
 }
 
