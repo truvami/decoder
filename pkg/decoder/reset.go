451ed6127772d1810b7501b1061fe762a0de56bb
Michael Beutler <35310806+michaelbeutler@users.noreply.github.com>
2025-04-24 13:53:54 +0200

    update reset.go

--- pkg/decoder/reset.go
+++ pkg/decoder/reset.go
@@ -3,10 +3,19 @@ package decoder
 type ResetReason string
 
 // TODO: Add more reset reasons and descriptions, check with Viktor
-const ResetReasonUnknown ResetReason = "unknown"
-const ResetReasonLrr1110Failure ResetReason = "lrr1110-failure"
-const ResetReasonWatchdog ResetReason = "watchdog"
-const ResetReasonPinReset ResetReason = "pin-reset"
-const ResetReasonSystemReset ResetReason = "system-reset"
-const ResetReasonOtherReset ResetReason = "other-reset"
-const ResetReasonPowerReset ResetReason = "power-reset"
+const (
+	// Device did reset for an unknown reason
+	ResetReasonUnknown ResetReason = "UNKNOWN"
+	// Device did reset because of LRR1110 failure
+	ResetReasonLrr1110FailCode ResetReason = "LRR1110_FAILURE"
+	// Device did reset because of a watchdog timeout
+	ResetReasonWatchdog ResetReason = "WATCHDOG"
+	// Device did reset because of a pin reset
+	ResetReasonPinReset ResetReason = "PIN_RESET"
+	// Device did reset because of a system reset
+	ResetReasonSystemReset ResetReason = "SYSTEM_RESET"
+	// Device did reset because of another reason
+	ResetReasonOtherReset ResetReason = "OTHER_RESET"
+	// Device did reset because of a power reset
+	ResetReasonPowerReset ResetReason = "POWER_RESET"
+)
