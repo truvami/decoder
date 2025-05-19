b2c2a89cf77899a1ecec988d52a37d17f7560536
simylein <67859356+simylein@users.noreply.github.com>
2025-05-19 11:22:35 +0000

    refactor(tagsl): port 7 now properly using pointers

--- pkg/decoder/tagsl/v1/port7.go
+++ pkg/decoder/tagsl/v1/port7.go
@@ -37,16 +37,16 @@ type Port7Payload struct {
 	Moving              bool      `json:"moving"`
 	Mac1                string    `json:"mac1"`
 	Rssi1               int8      `json:"rssi1" validate:"gte=-120,lte=-20"`
-	Mac2                string    `json:"mac2"`
-	Rssi2               int8      `json:"rssi2" validate:"gte=-120,lte=-20"`
-	Mac3                string    `json:"mac3"`
-	Rssi3               int8      `json:"rssi3" validate:"gte=-120,lte=-20"`
-	Mac4                string    `json:"mac4"`
-	Rssi4               int8      `json:"rssi4" validate:"gte=-120,lte=-20"`
-	Mac5                string    `json:"mac5"`
-	Rssi5               int8      `json:"rssi5" validate:"gte=-120,lte=-20"`
-	Mac6                string    `json:"mac6"`
-	Rssi6               int8      `json:"rssi6" validate:"gte=-120,lte=-20"`
+	Mac2                *string   `json:"mac2"`
+	Rssi2               *int8     `json:"rssi2" validate:"gte=-120,lte=-20"`
+	Mac3                *string   `json:"mac3"`
+	Rssi3               *int8     `json:"rssi3" validate:"gte=-120,lte=-20"`
+	Mac4                *string   `json:"mac4"`
+	Rssi4               *int8     `json:"rssi4" validate:"gte=-120,lte=-20"`
+	Mac5                *string   `json:"mac5"`
+	Rssi5               *int8     `json:"rssi5" validate:"gte=-120,lte=-20"`
+	Mac6                *string   `json:"mac6"`
+	Rssi6               *int8     `json:"rssi6" validate:"gte=-120,lte=-20"`
 }
 
 var _ decoder.UplinkFeatureBase = &Port7Payload{}
@@ -62,45 +62,45 @@ func (p Port7Payload) GetTimestamp() *time.Time {
 func (p Port7Payload) GetAccessPoints() []decoder.AccessPoint {
 	accessPoints := []decoder.AccessPoint{}
 
-	if p.Mac1 != "" {
+	if p.Mac1 != "" && p.Rssi1 != 0 {
 		accessPoints = append(accessPoints, decoder.AccessPoint{
 			MAC:  p.Mac1,
 			RSSI: p.Rssi1,
 		})
 	}
 
-	if p.Mac2 != "" {
+	if p.Mac2 != nil && p.Rssi2 != nil {
 		accessPoints = append(accessPoints, decoder.AccessPoint{
-			MAC:  p.Mac2,
-			RSSI: p.Rssi2,
+			MAC:  *p.Mac2,
+			RSSI: *p.Rssi2,
 		})
 	}
 
-	if p.Mac3 != "" {
+	if p.Mac3 != nil && p.Rssi3 != nil {
 		accessPoints = append(accessPoints, decoder.AccessPoint{
-			MAC:  p.Mac3,
-			RSSI: p.Rssi3,
+			MAC:  *p.Mac3,
+			RSSI: *p.Rssi3,
 		})
 	}
 
-	if p.Mac4 != "" {
+	if p.Mac4 != nil && p.Rssi4 != nil {
 		accessPoints = append(accessPoints, decoder.AccessPoint{
-			MAC:  p.Mac4,
-			RSSI: p.Rssi4,
+			MAC:  *p.Mac4,
+			RSSI: *p.Rssi4,
 		})
 	}
 
-	if p.Mac5 != "" {
+	if p.Mac5 != nil && p.Rssi5 != nil {
 		accessPoints = append(accessPoints, decoder.AccessPoint{
-			MAC:  p.Mac5,
-			RSSI: p.Rssi5,
+			MAC:  *p.Mac5,
+			RSSI: *p.Rssi5,
 		})
 	}
 
-	if p.Mac6 != "" {
+	if p.Mac6 != nil && p.Rssi6 != nil {
 		accessPoints = append(accessPoints, decoder.AccessPoint{
-			MAC:  p.Mac6,
-			RSSI: p.Rssi6,
+			MAC:  *p.Mac6,
+			RSSI: *p.Rssi6,
 		})
 	}
 
