4ebf554620e7227beb5693dddbeb0d55bd8d36fd
Niko Kriznik <nikok@globtim.com>
2025-08-12 15:02:57 +0000

    Removed altitude is/isn't zero checks from Traxmate metrics

--- pkg/solver/loracloud/loracloud.go
+++ pkg/solver/loracloud/loracloud.go
@@ -296,20 +296,17 @@ func (m LoracloudClient) DeliverUplinkMessage(devEui string, uplinkMsg UplinkMsg
 		loracloudPositionEstimateNoCapturedAtSetCounter.WithLabelValues(metricDevEui).Inc()
 	}
 	if uplinkResponse.GetLatitude() == 0 &&
-		uplinkResponse.GetLongitude() == 0 &&
-		uplinkResponse.GetAltitude() == 0 {
+		uplinkResponse.GetLongitude() == 0 {
 		loracloudPositionEstimateZeroCoordinatesSetCounter.WithLabelValues(metricDevEui).Inc()
 	}
 	if uplinkResponse.GetTimestamp() == nil &&
 		uplinkResponse.GetLatitude() == 0 &&
-		uplinkResponse.GetLongitude() == 0 &&
-		uplinkResponse.GetAltitude() == 0 {
+		uplinkResponse.GetLongitude() == 0 {
 		loracloudPositionEstimateNoCapturedAtSetAndZeroCoordinatesSetCounter.WithLabelValues(metricDevEui).Inc()
 	}
 	if uplinkResponse.GetTimestamp() == nil &&
 		uplinkResponse.GetLatitude() != 0 &&
-		uplinkResponse.GetLongitude() != 0 &&
-		uplinkResponse.GetAltitude() != 0 {
+		uplinkResponse.GetLongitude() != 0 {
 		loracloudPositionEstimateNoCapturedAtSetWithValidCoordinatesCounter.WithLabelValues(metricDevEui).Inc()
 	}
 
